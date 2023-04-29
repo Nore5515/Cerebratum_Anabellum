30e7688e42d1096de8fe2f6842c973ff7ed8855d
Noah Reed <Nore5515@gmail.com>
2023-04-29 11:57:41 -0500

    shadow fixing

--- Assets/Scripts/Prefabs/ShadowScript.cs
+++ Assets/Scripts/Prefabs/ShadowScript.cs
@@ -20,6 +20,9 @@ public class ShadowScript : MonoBehaviour
             float bulletHeight = pairedBullet.transform.position.y;
             if (bulletHeight != float.NaN && bulletHeight > 0.0f){
                 this.transform.localScale = new Vector3(Mathf.Log(bulletHeight+1, 2.0f), 0.0f, Mathf.Log(bulletHeight+1, 2.0f));
+                float newAlpha = (255 - (255 * (Mathf.Log(bulletHeight+1, 1.5f) / 10))) / 255.0f;
+                Color c = new Color(0.0f, 0.0f, 0.0f, newAlpha);
+                this.transform.Find("Circle").gameObject.GetComponent<SpriteRenderer>().color = c;
             }
             else{
                 Destroy(this.gameObject);
