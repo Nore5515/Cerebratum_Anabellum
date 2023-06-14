a6f4f558fb560bbd097ab9604bbc9f64ee7dd955
Noah Reed <Nore5515@gmail.com>
2023-06-14 14:23:20 -0500

    repositioning scene and added lighting

---
+++ Assets/Scripts/BuildingHandler.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingHandler : MonoBehaviour
+{
+
+    public List<BuildingSlot> buildingSlots = new List<BuildingSlot>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
