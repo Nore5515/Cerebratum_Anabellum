78da268864cead638da57d759c4077790d0c2b1e
Noah Reed <Nore5515@gmail.com>
2023-06-14 12:03:36 -0500

    refitting scene tree

--- Assets/Scripts/Env/SpawnerHandler.cs
+++ Assets/Scripts/Env/SpawnerHandler.cs
@@ -6,9 +6,12 @@ using UnityEngine.UI;
 public class SpawnerHandler : MonoBehaviour
 {
     Button spidertankSpawner;
+    Button infSpawner;
     public GameObject spidertankSpawnerPrefab;
+    public GameObject infSpawnerPrefab;
 
-    int stSpawnerCost;
+    int stSpawnerCost = 10;
+    int infSpawnerCost = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +22,32 @@ public class SpawnerHandler : MonoBehaviour
     void InitializeUI()
     {
         GameObject buildings = GameObject.Find("Canvas/Constructions/Buildings");
-        
+
         // Buttons
         spidertankSpawner = buildings.transform.Find("AddSpidertankSpawner").gameObject.GetComponent<Button>();
-        spidertankSpawner.onClick.AddListener(delegate { AddSpidertankSpawner();});
+        spidertankSpawner.onClick.AddListener(delegate { AddSpidertankSpawner(); });
+        spidertankSpawner = buildings.transform.Find("RAddSpawner").gameObject.GetComponent<Button>();
+        spidertankSpawner.onClick.AddListener(delegate { AddInfSpawner(); });
+    }
+
+    void AddInfSpawner()
+    {
+        Debug.Log("Creating new spawner! Count is : " + SpawnerTracker.redSpawnerObjs.Count);
+
+        if (SpawnerTracker.redSpawnerObjs.Count <= 2)
+        {
+            if (TeamStats.RedPoints >= infSpawnerCost)
+            {
+                TeamStats.RedPoints -= infSpawnerCost;
+                Debug.Log("New spawner about to be added. Count is : " + SpawnerTracker.redSpawnerObjs.Count);
+                Vector3 newPos = SpawnerTracker.redSpawnerObjs[0].transform.position;
+                newPos.z += (16.0f * (SpawnerTracker.redSpawnerObjs.Count - 1.5f));
+                GameObject newObj = Instantiate(infSpawnerPrefab, newPos, Quaternion.identity) as GameObject;
+                newObj.GetComponent<Structure>().type = "spawn";
+                SpawnerTracker.redSpawnerObjs.Add(newObj);
+                Debug.Log("New spawner added. Count is : " + SpawnerTracker.redSpawnerObjs.Count);
+            }
+        }
     }
 
     void AddSpidertankSpawner()
@@ -48,7 +73,7 @@ public class SpawnerHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    
+
 }
