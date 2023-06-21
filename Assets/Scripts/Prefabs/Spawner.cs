357af25810507d513286268fd3db8f580d86c8ec
Noah Reed <Nore5515@gmail.com>
2023-06-21 16:36:22 -0500

    Moved class out of spawner

--- Assets/Scripts/Prefabs/Spawner.cs
+++ Assets/Scripts/Prefabs/Spawner.cs
@@ -16,26 +16,6 @@ public class Spawner : Structure
 
     public string spawnerTeam = "RED";
 
-    public class SpawnedUnitStats
-    {
-        public float startingFireDelay = 2.0f;
-        public float startingUnitRange = 3.0f;
-        public float startingSpawnTime = 3.0f;
-        public float MAX_UNIT_FIRE_RATE = 0.5f;
-        public float MAX_UNIT_RANGE = 6.0f;
-        public float MAX_UNIT_SPAWN_RATE = 1.0f;
-        public float fireDelay;
-        public float unitRange;
-        public float spawnTime;
-
-        public void ResetToStartingStats()
-        {
-            fireDelay = startingFireDelay;
-            unitRange = startingUnitRange;
-            spawnTime = startingSpawnTime;
-        }
-    }
-
     SpawnedUnitStats spawnedUnitStats = new SpawnedUnitStats();
 
     // NEW STUFF
