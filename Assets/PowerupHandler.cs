4e7ecbcf0c266b07223d02d3831507017919e810
Noah Reed <Nore5515@gmail.com>
2023-09-29 21:13:45 -0500

    super tacky but funny fireball prototype

---
+++ Assets/PowerupHandler.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.UI.CanvasScaler;
+
+public class PowerupHandler : MonoBehaviour
+{
+    public GameObject fireballPlaceholder;
+
+    Color fireballMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fireballMat = fireballPlaceholder.GetComponent<Renderer>().material.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 hitPos = RayHitFloorPosition();
+        if (hitPos != new Vector3(0.0f, 0.0f, 0.0f))
+        {
+            fireballPlaceholder.transform.position = hitPos;
+        }
+
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            fireballMat.a = 0.8f;
+            fireballPlaceholder.GetComponent<Renderer>().material.color = fireballMat;
+            VaporizePoorFools();
+        }
+        else
+        {
+            fireballMat.a = 0.2f;
+            fireballPlaceholder.GetComponent<Renderer>().material.color = fireballMat;
+        }
+    }
+
+    void VaporizePoorFools()
+    {
+        Collider[] potentialVictims = GetAllCollidersInSphere();
+        List<GameObject> confirmedVictims = new List<GameObject>();
+        foreach (var hitCollider in potentialVictims)
+        {
+            if (hitCollider.gameObject.tag == "unit")
+            {
+                confirmedVictims.Add(hitCollider.gameObject);
+            }
+        }
+
+        foreach (GameObject victim in confirmedVictims)
+        {
+            if(victim.GetComponent<Unit>().DealDamage(999) <= 0)
+            {
+                Destroy(victim.GetComponent<Unit>().gameObject);
+            }
+        }
+    }
+
+    Collider[] GetAllCollidersInSphere()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(fireballPlaceholder.transform.position, 60);
+        return hitColliders;
+    }
+
+    Vector3 RayHitFloorPosition()
+    {
+        RayObj rayObj = new RayObj();
+        rayObj.ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        rayObj.hit = GetHitAgainstLayer(rayObj, "Floor");
+        if (rayObj.hit.transform != null)
+        {
+            return rayObj.hit.transform.position;
+        }
+        return new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    RaycastHit GetHitAgainstLayer(RayObj rayObj, string maskName)
+    {
+        RaycastHit caughtHit;
+        Physics.Raycast(rayObj.ray, out caughtHit, Mathf.Infinity, GenerateLayerMask(maskName));
+        return caughtHit;
+    }
+
+    LayerMask GenerateLayerMask(string maskName)
+    {
+        LayerMask mask = LayerMask.GetMask(maskName);
+        return mask;
+    }
+
+}
