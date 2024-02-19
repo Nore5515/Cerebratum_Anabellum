edd6f349c96cb2300001c92a987ff973dc61c071
Noah Reed <Nore5515@gmail.com>
2024-02-18 18:49:58 -0600

    tech tree basics

--- Assets/Scripts/TechTree.cs
+++ Assets/Scripts/TechTree.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TechTree : MonoBehaviour
@@ -7,14 +8,31 @@ public class TechTree : MonoBehaviour
     [SerializeField]
     GameObject techTreeScreen;
 
+    [SerializeField]
+    TextMeshProUGUI text_spawnRate;
+    [SerializeField]
+    TextMeshProUGUI text_range;
+    [SerializeField]
+    TextMeshProUGUI text_rateOfFire;
+
     public void SetScreen(GameObject techTreeInstance)
     {
         techTreeScreen = techTreeInstance;
     }
 
-    public void ClickButton()
+    public void Button_SpawnRate()
     {
         Debug.Log("Hello, world!");
+        text_spawnRate.text = "Pressed!";
+    }
+
+    public void Button_Range()
+    {
+        text_range.text = "Pressed!";
+    }
+    public void Button_RateOfFire()
+    {
+        text_rateOfFire.text = "Pressed!";
     }
 
     private void Update()
