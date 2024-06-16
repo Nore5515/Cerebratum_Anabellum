33718f539575b63f945f9e01bab9222412b58da6
Noah Reed <Nore5515@gmail.com>
2024-06-16 16:57:41 -0700

    camapign menu basic setup

---
+++ Assets/CampaignScene.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class CampaignScene : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI bestTime;
+
+    string selectedLevel = "";
+    // first value is screen level
+    // second value is actual level name
+    Dictionary<string, string> visualToActualNamePairs = new();
+
+    public void SelectLevel(string newLevel)
+    {
+        selectedLevel = newLevel;
+        UpdateLevelPreview();
+    }
+
+    void UpdateLevelPreview()
+    {
+        bestTime.text = GetBestTime().ToString();
+    }
+
+    public void StartLevel()
+    {
+        if (visualToActualNamePairs.ContainsKey(selectedLevel))
+        {
+            SceneManager.LoadScene(visualToActualNamePairs[selectedLevel], LoadSceneMode.Single);
+        }
+        else
+        {
+            Debug.LogError("ERROR: level " + selectedLevel + " not found!");
+        }
+    }
+
+    // TODO: Implement
+    float GetBestTime()
+    {
+        // Access best times
+        // Load best time by selectedLevel
+        return 0.0f;
+    }
+}
